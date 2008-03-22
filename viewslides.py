29d45d4f3c16f922cbd6d62608aa158b4096252f
James Simmons <ja.simmons@sbcglobal.net>
2008-03-22 10:41:59 -0500

    modified:   activity/activity.info modified:   viewslides.py Add sharing and power management code to ViewSlides.

--- viewslides.py
+++ viewslides.py
@@ -1,21 +1,71 @@
 #! /usr/bin/env python
+
+# Copyright (C) 2008 James D. Simmons
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 import sys
 import os
+import logging
+import tempfile
+import time
 import zipfile
 import pygtk
 import gtk
+import string
 import pygame, pygame.display
 from pygame.locals import *
 from sugar.activity import activity
+from sugar import network
+from sugar.datastore import datastore
 from readtoolbar import ReadToolbar
 from gettext import gettext as _
+import pango
+import dbus
+import gobject
+import hippo
+import telepathy
 
-i=0
+_HARDWARE_MANAGER_INTERFACE = 'org.laptop.HardwareManager'
+_HARDWARE_MANAGER_SERVICE = 'org.laptop.HardwareManager'
+_HARDWARE_MANAGER_OBJECT_PATH = '/org/laptop/HardwareManager'
+
+_TOOLBAR_READ = 1
+_SOCKET_TYPE_IPv4 = 2
+_SOCKET_ACCESS_CONTROL_LOCALHOST = 0
+
+_logger = logging.getLogger('view-slides')
+
+class ReadHTTPRequestHandler(network.ChunkedGlibHTTPRequestHandler):
+    def translate_path(self, path):
+        return self.server._filepath
+
+class ReadHTTPServer(network.GlibTCPServer):
+    def __init__(self, server_address, filepath):
+        self._filepath = filepath
+        network.GlibTCPServer.__init__(self, server_address, ReadHTTPRequestHandler)
+
+READ_STREAM_SERVICE = 'read-activity-http'
 
 class ViewSlidesActivity(activity.Activity):
     def __init__(self, handle):
         "The entry point to the Activity"
         activity.Activity.__init__(self, handle)
+
+        self._fileserver = None
+        self._object_id = handle.object_id
+
         self.connect("expose_event", self.area_expose_cb)
         self.connect("key_press_event", self.keypress_cb)
         toolbox = activity.ActivityToolbox(self)
@@ -28,6 +78,57 @@ class ViewSlidesActivity(activity.Activity):
         self.set_canvas(self.image)
         self.show_image("ViewSlides.jpg")
         self._read_toolbar.set_activity(self)
+
+        # Set up for idle suspend
+        self._idle_timer = 0
+        self._service = None
+
+        # start with sleep off
+        self._sleep_inhibit = True
+
+        fname = os.path.join('/etc', 'inhibit-ebook-sleep')
+        if not os.path.exists(fname):
+            try:
+                bus = dbus.SystemBus()
+                proxy = bus.get_object(_HARDWARE_MANAGER_SERVICE,
+                                       _HARDWARE_MANAGER_OBJECT_PATH)
+                self._service = dbus.Interface(proxy, _HARDWARE_MANAGER_INTERFACE)
+                scrolled.props.vadjustment.connect("value-changed", self._user_action_cb)
+                scrolled.props.hadjustment.connect("value-changed", self._user_action_cb)
+                self.connect("focus-in-event", self._focus_in_event_cb)
+                self.connect("focus-out-event", self._focus_out_event_cb)
+                self.connect("notify::active", self._now_active_cb)
+
+                logging.debug('Suspend on idle enabled')
+            except dbus.DBusException, e:
+                _logger.info('Hardware manager service not found, no idle suspend.')
+        else:
+            logging.debug('Suspend on idle disabled')
+    
+        # start on the read toolbar
+        self.toolbox.set_current_toolbar(_TOOLBAR_READ)
+        self.unused_download_tubes = set()
+        self._want_document = True
+        self.connect("shared", self._shared_cb)
+        h = hash(self._activity_id)
+        self.port = 1024 + (h % 64511)
+
+        if handle.uri:
+            self._load_document(handle.uri)
+
+        self.is_received_document = False
+        
+        if self._shared_activity:
+            # We're joining
+            if self.get_shared():
+                # Already joined for some reason, just get the document
+                self._joined_cb(self)
+            else:
+                # Wait for a successful join before trying to get the document
+                self.connect("joined", self._joined_cb)
+        # uncomment this and adjust the path for easier testing
+        #else:
+        #    self._load_document('file:///home/smcv/tmp/test.pdf')
 
     def keypress_cb(self, widget, event):
         "Respond when the user presses Escape or one of the arrow keys"
@@ -130,6 +231,10 @@ class ViewSlidesActivity(activity.Activity):
 
     def _load_document(self, file_path):
         "Read the Zip file containing the images"
+        self.shared_file = open(file_path,"r")
+        # We open it both as a regular file and as a Zip file
+        # The regular file will be copied to the Journal if
+        # we received the file from elsewhere.
         self.zf = zipfile.ZipFile(file_path, 'r')
         self.image_files = self.zf.namelist()
         self.image_files.sort()
@@ -140,7 +245,193 @@ class ViewSlidesActivity(activity.Activity):
         os.remove(currentFileName)
         self._read_toolbar.set_total_pages(len(self.image_files))
         self._read_toolbar.set_current_page(self.page)
+        # We've got the document, so if we're a shared activity, offer it
+        if self.get_shared():
+            self.watch_for_tubes()
+            self._share_document()
 
     def write_file(self, file_path):
         "Save meta data for the file."
+        if self.is_received_document == True:
+            # This document was given to us by someone, so we have
+            # to save it to the Journal.
+            filebytes = self.shared_file.read()
+            f = open(filename, 'w')
+            try:
+                f.write(filebytes)
+            finally:
+                f.close
+
         self.metadata['current_image'] =str(self.page)
+
+    # The code from here on down is for sharing.
+    def _download_result_cb(self, getter, tempfile, suggested_name, tube_id):
+        del self.unused_download_tubes
+
+        _logger.debug("Got document %s (%s) from tube %u",
+                      tempfile, suggested_name, tube_id)
+        self._load_document(tempfile)
+        _logger.debug("Saving %s to datastore...", tempfile)
+        self.save()
+
+    def _download_progress_cb(self, getter, bytes_downloaded, tube_id):
+        # FIXME: signal the expected size somehow, so we can draw a progress
+        # bar
+        _logger.debug("Downloaded %u bytes from tube %u...",
+                      bytes_downloaded, tube_id)
+
+    def _download_error_cb(self, getter, err, tube_id):
+        _logger.debug("Error getting document from tube %u: %s",
+                      tube_id, err)
+        self._want_document = True
+        gobject.idle_add(self._get_document)
+
+    def _download_document(self, tube_id):
+        # FIXME: should ideally have the CM listen on a Unix socket
+        # instead of IPv4 (might be more compatible with Rainbow)
+        chan = self._shared_activity.telepathy_tubes_chan
+        iface = chan[telepathy.CHANNEL_TYPE_TUBES]
+        addr = iface.AcceptStreamTube(tube_id,
+                # telepathy.SOCKET_ADDRESS_TYPE_IPV4,
+                _SOCKET_TYPE_IPv4,
+                # telepathy.SOCKET_ACCESS_CONTROL_LOCALHOST, 0,
+                _SOCKET_ACCESS_CONTROL_LOCALHOST, 0,
+                utf8_strings=True)
+        _logger.debug('Accepted stream tube: listening address is %r', addr)
+        # SOCKET_ADDRESS_TYPE_IPV4 is defined to have addresses of type '(sq)'
+        assert isinstance(addr, dbus.Struct)
+        assert len(addr) == 2
+        assert isinstance(addr[0], str)
+        assert isinstance(addr[1], (int, long))
+        assert addr[1] > 0 and addr[1] < 65536
+        port = int(addr[1])
+
+        getter = network.GlibURLDownloader("http://%s:%d/document"
+                                           % (addr[0], port))
+        getter.connect("finished", self._download_result_cb, tube_id)
+        getter.connect("progress", self._download_progress_cb, tube_id)
+        getter.connect("error", self._download_error_cb, tube_id)
+        _logger.debug("Starting download to %s...", self._jobject.file_path)
+        getter.start(self._jobject.file_path)
+        self.is_received_document = True
+        return False
+
+    def _get_document(self):
+        if not self._want_document:
+            return False
+
+        # Assign a file path to download if one doesn't exist yet
+        if not self._jobject.file_path:
+            self._jobject.file_path = os.path.join(tempfile.gettempdir(), '%i' % time.time())
+            self._owns_file = True
+
+        # Pick an arbitrary tube we can try to download the document from
+        try:
+            tube_id = self.unused_download_tubes.pop()
+        except (ValueError, KeyError), e:
+            _logger.debug('No tubes to get the document from right now: %s',
+                          e)
+            return False
+
+        # Avoid trying to download the document multiple times at once
+        self._want_document = False
+        gobject.idle_add(self._download_document, tube_id)
+        return False
+
+    def _joined_cb(self, also_self):
+        self.watch_for_tubes()
+        gobject.idle_add(self._get_document)
+
+    def _share_document(self):
+        # FIXME: should ideally have the fileserver listen on a Unix socket
+        # instead of IPv4 (might be more compatible with Rainbow)
+
+        # FIXME: there is an issue with the Activity class and Read that makes
+        # the file disappear; probably related to write_file not writing a
+        # file. This is a dirty fix and should be improved later.
+        if self._jobject is None:
+            self._jobject = datastore.get(self._object_id)
+        elif not os.path.exists(self._jobject.get_file_path()):
+            self._jobject.destroy()
+            self._jobject = datastore.get(self._object_id)
+
+        self._fileserver = ReadHTTPServer(("", self.port),
+            self._jobject.get_file_path())
+
+        # Make a tube for it
+        chan = self._shared_activity.telepathy_tubes_chan
+        iface = chan[telepathy.CHANNEL_TYPE_TUBES]
+        self._fileserver_tube_id = iface.OfferStreamTube(READ_STREAM_SERVICE,
+                {},
+                # telepathy.SOCKET_ADDRESS_TYPE_IPV4,
+                _SOCKET_TYPE_IPv4,
+                ('127.0.0.1', dbus.UInt16(self.port)),
+                # telepathy.SOCKET_ACCESS_CONTROL_LOCALHOST, 0)
+                _SOCKET_ACCESS_CONTROL_LOCALHOST, 0)
+
+    def watch_for_tubes(self):
+        tubes_chan = self._shared_activity.telepathy_tubes_chan
+
+        tubes_chan[telepathy.CHANNEL_TYPE_TUBES].connect_to_signal('NewTube',
+            self._new_tube_cb)
+        tubes_chan[telepathy.CHANNEL_TYPE_TUBES].ListTubes(
+            reply_handler=self._list_tubes_reply_cb,
+            error_handler=self._list_tubes_error_cb)
+
+    def _new_tube_cb(self, tube_id, initiator, tube_type, service, params,
+                     state):
+        _logger.debug('New tube: ID=%d initator=%d type=%d service=%s '
+                      'params=%r state=%d', tube_id, initiator, tube_type,
+                      service, params, state)
+        if service == READ_STREAM_SERVICE:
+            _logger.debug('I could download from that tube')
+            self.unused_download_tubes.add(tube_id)
+            # if no download is in progress, let's fetch the document
+            if self._want_document:
+                gobject.idle_add(self._get_document)
+
+    def _list_tubes_reply_cb(self, tubes):
+        for tube_info in tubes:
+            self._new_tube_cb(*tube_info)
+
+    def _list_tubes_error_cb(self, e):
+        _logger.error('ListTubes() failed: %s', e)
+
+    def _shared_cb(self, activity):
+        # We initiated this activity and have now shared it, so by
+        # definition we have the file.
+        _logger.debug('Activity became shared')
+        self.watch_for_tubes()
+        self._share_document()
+
+    # From here down is power management stuff.
+    def _now_active_cb(self, widget, pspec):
+        if self.props.active:
+            # Now active, start initial suspend timeout
+            if self._idle_timer > 0:
+                gobject.source_remove(self._idle_timer)
+            self._idle_timer = gobject.timeout_add(15000, self._suspend_cb)
+            self._sleep_inhibit = False
+        else:
+            # Now inactive
+            self._sleep_inhibit = True
+
+    def _focus_in_event_cb(self, widget, event):
+        self._sleep_inhibit = False
+        self._user_action_cb(self)
+
+    def _focus_out_event_cb(self, widget, event):
+        self._sleep_inhibit = True
+
+    def _user_action_cb(self, widget):
+        if self._idle_timer > 0:
+            gobject.source_remove(self._idle_timer)
+        self._idle_timer = gobject.timeout_add(5000, self._suspend_cb)
+
+    def _suspend_cb(self):
+        # If the machine has been idle for 5 seconds, suspend
+        self._idle_timer = 0
+        if not self._sleep_inhibit:
+            self._service.set_kernel_suspend()
+        return False
+        
