324025a58476bf3e6a48b3003434939584634791
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2018-04-10 00:22:12 +0530

    Fix PyGI warnings

--- viewslides.py
+++ viewslides.py
@@ -19,6 +19,8 @@ import logging
 import time
 import zipfile
 from zipfile import BadZipfile
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import GdkPixbuf
 from gi.repository import Gdk
